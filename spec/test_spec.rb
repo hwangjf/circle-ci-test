430b3d2f152c31cac015cf7c0654fcea709ade06
hwangjf <jeffreyhwang90@gmail.com>
2019-08-28 16:27:07 -0400

    testing

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
+require 'selenium-webdriver'
 
 describe 'thing' do
   
   it 'should do this' do
     puts 'hi'
+    driver = Selenium::WebDriver.for :chrome
+    driver.navigate.to "http://google.com"
+
+    element = driver.find_element(:name, 'q')
+    element.send_keys "Hello WebDriver!"
+    element.submit
+
+    puts driver.title
+
+    driver.quit
     expect(true).to equal true
   end
 end
